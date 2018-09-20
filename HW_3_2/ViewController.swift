bf967c1c8fd5df9ca7808ff0f9503602f952a6e1
user144683 <user144683@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-09-20 10:31:37 -0400

    Initial Commit

---
+++ HW_3_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HW_3_2
+//
+//  Created by user144683 on 9/20/18.
+//  Copyright © 2018 JustinDowty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
